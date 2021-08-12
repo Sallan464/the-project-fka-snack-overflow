b3220570e92246beb175358efa3f615e5da3add3
cerise-at <cerise.athompson@gmail.com>
2021-08-12 11:26:23 +0100

    added form show and hide functions

--- client/index.js
+++ client/index.js
@@ -22,3 +22,25 @@ function renderPosts(posts) {
 function newPostSubmitHandler() {
 
 }
+
+
+//closes form and calls create post
+function newSnaccFormHandler(e){
+    createPost()
+    closeForm()
+}
+
+//sends data to teh server which would then display it 
+function createPost(){
+    //snaccrr has some of this code
+}
+
+//opens the form on click
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+//closes it after submission
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
